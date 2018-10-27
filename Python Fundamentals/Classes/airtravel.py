2c9e6cc504609d77dd3342c9be8a855d4f3668ef
Guthrie Hayward <guthriehayward@gmail.com>
2018-10-27 07:02:28 -0700

    Fixed bug in console_card_printer preventing function from working at runtime

--- Python Fundamentals/Classes/airtravel.py
+++ Python Fundamentals/Classes/airtravel.py
@@ -141,7 +141,7 @@ def console_card_printer(passenger, seat, flight_number, aircraft):
              "  Seat: {2}"      \
              "  Aircraft: {3}"  \
              " |".format(passenger, flight_number, seat, aircraft)
-    banner = '+' = '-' * (len(output) - 2) + '+'
+    banner = '+' + '-' * (len(output) - 2) + '+'
     border = '|' + ' ' * (len(output) -2) + '|'
     lines = [banner, border, output, border, banner]
     card = '\n'.join(lines)
